1d005464e7b792d1e8b01b8f6c3a7df2806b2562
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-11 20:18:51 -0300

    itens do desafio da parte um

---
+++ Projeto/app.js
@@ -0,0 +1,19 @@
+alert('Boas vindas ao nosso site'); // Item 1
+let nome = 'Lua'; // Item 2 
+let idade = 25; // Item 3
+let numeroDeVendas = 50; // Item 4
+let saldoDisponível = 1000; //Item 5
+alert('Erro! Preencha todos os campos'); // Item 6
+
+//Item 7
+let mensagemDeErro = 'Erro! Preencha todos os campos'
+alert(mensagemDeErro);
+
+//Item 8
+nome = prompt('Digite seu nome');
+
+//Item 9
+idade = prompt('Digite sua idade');
+if (idade >= 18){
+    alert('Pode tirar a habilitação!')
+}
