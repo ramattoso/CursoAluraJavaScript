b38bc844e4caf70bd05dff1dc18790497ae7276c
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-11 20:21:14 -0300

    feito na primeira parte do curso

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,19 +1,7 @@
-alert('Boas vindas ao nosso site'); // Item 1
-let nome = 'Lua'; // Item 2 
-let idade = 25; // Item 3
-let numeroDeVendas = 50; // Item 4
-let saldoDisponível = 1000; //Item 5
-alert('Erro! Preencha todos os campos'); // Item 6
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
 
-//Item 7
-let mensagemDeErro = 'Erro! Preencha todos os campos'
-alert(mensagemDeErro);
-
-//Item 8
-nome = prompt('Digite seu nome');
-
-//Item 9
-idade = prompt('Digite sua idade');
-if (idade >= 18){
-    alert('Pode tirar a habilitação!')
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (5)');
 }
