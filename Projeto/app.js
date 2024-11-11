885c5d4051688876d23b0eef4e4895b33f992ae9
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-11 20:54:40 -0300

    desafios da parte dois do curso

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,19 +1,33 @@
-alert('Boas vindas ao nosso site'); // Item 1
-let nome = 'Lua'; // Item 2 
-let idade = 25; // Item 3
-let numeroDeVendas = 50; // Item 4
-let saldoDisponível = 1000; //Item 5
-alert('Erro! Preencha todos os campos'); // Item 6
-
-//Item 7
-let mensagemDeErro = 'Erro! Preencha todos os campos'
-alert(mensagemDeErro);
-
-//Item 8
-nome = prompt('Digite seu nome');
-
-//Item 9
-idade = prompt('Digite sua idade');
-if (idade >= 18){
-    alert('Pode tirar a habilitação!')
+//Item 1
+let dia = prompt('Hoje é qual dia da semana?');
+if (dia == 'Sábado' || dia == 'Domingo'){
+    alert('Bom fim de semana!')
+} else {
+    alert('Boa semana!')
 }
+
+//Item 2
+let numeroDigitado = prompt('Digite um número real qualquer')
+if (numeroDigitado >=0){
+    alert(`O número ${numeroDigitado} é positivo!`)
+}else{
+    alert(`O número ${numeroDigitado} é negativo!`)
+}
+
+//Item 3
+
+numeroDigitado = prompt('Digite qualquer número')
+if (numeroDigitado >= 100){
+    alert('Parabéns, você venceu!');
+}else {
+    alert('Tente novamente para ganhar.')
+}
+
+//Item 4
+
+let saldo = 'R$ 5.000.000';
+alert(`O saldo da sua conta é ${saldo}`);
+
+//Item 5
+let nome = prompt('Digite seu nome')
+alert('Seja bem vindo(a), ' + nome)
