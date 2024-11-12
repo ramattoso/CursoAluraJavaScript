9bfd00fbc8c25e7fe4b86836ddfacad57c849fe2
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-12 19:41:24 -0300

    parte 3

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,10 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+let chute;
+let tentativas = 1;
+// enquanto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    tentativas = tentativas + 1;
 }
