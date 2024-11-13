0c7ad65b55f8504608433772b4feee8c308d5554
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-12 21:45:31 -0300

    primeiros passos do desafio

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,23 +1,38 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 100;
-let numeroSecreto = parseInt(Math.random()*numeroMaximo + 1);
-let chute;
+alert('Boas vindas ao jogo do número secreto. São seis fases! Boa sorte (:');
+let numeroSecreto, chute, novaFase;
+let totalTentativas = 0;
+let fase = 1;
+let numeroMaximo = [10,100,1000,10000,100000,1000000];
 let tentativas = 1;
-// enquanto
-while (chute != numeroSecreto){
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
+let continuar = 'Sim';
+while (fase < 6){
+    numeroSecreto = parseInt(Math.random()*numeroMaximo[fase-1] + 1);
+    while (chute != numeroSecreto){
+        chute = prompt(`Esta é a fase ${fase} do jogo. Escolha um número entre 1 e ${numeroMaximo[fase-1]} `);
+        if (chute == numeroSecreto) {
         break;
-    } else {
-        if (chute > numeroSecreto){
-            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`);
+            if (chute > numeroSecreto){
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
         }
+        tentativas++;
     }
-    tentativas++;
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+    novaFase = fase + 1;
+    continuar = prompt(`Deseja passar para a fase ${novaFase}? Digite Sim ou Não.`)
+    totalTentativas = totalTentativas + tentativas;
+    if(continuar == 'Sim'){
+        fase++;
+        tentativas = 1;
+    }else {break;}
 }
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+let palavraFases = fase > 1? 'fases' :'fase'
+let palavraTentativa = totalTentativas > 1? 'tentativas' : 'tentativa'
+alert(`No total você usou ${totalTentativas} ${palavraTentativa} e passou por ${fase} ${palavraFases}.`)
 
+document.getElementById("titulo").innerHTML = 'Resultado do jogo!'
+document.getElementById("mensagem").innerHTML = `No total você usou ${totalTentativas} ${palavraTentativa} e passou por ${fase} ${palavraFases}.`
---
+++ README.md
@@ -0,0 +1,5 @@
+# Desafio
+
+O curso não propôs nenhum desafio específico. Então, eu resolvi me desafiar. 
+
+A proposta é criar uma página html para o jogo de adivinhar o número que tenha fases, ou seja, após adivinhar o número o jogador pode escolher por ir para uma fase mais dificil. 
