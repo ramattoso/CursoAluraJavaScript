dc762413b49198f601372da64166a51d9e6fb4c3
ramattoso <100003127+ramattoso@users.noreply.github.com>
2024-11-13 19:57:23 -0300

    adicionando tratamento de erros

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,38 +1,51 @@
 alert('Boas vindas ao jogo do número secreto. São seis fases! Boa sorte (:');
-let numeroSecreto, chute, novaFase;
+let numeroSecreto, novaFase;
+let chute = -1;
 let totalTentativas = 0;
 let fase = 1;
 let numeroMaximo = [10,100,1000,10000,100000,1000000];
 let tentativas = 1;
 let continuar = 'Sim';
-while (fase < 6){
+while (fase < 6 && chute != null){
     numeroSecreto = parseInt(Math.random()*numeroMaximo[fase-1] + 1);
-    while (chute != numeroSecreto){
+    while (chute != null && chute != numeroSecreto){
         chute = prompt(`Esta é a fase ${fase} do jogo. Escolha um número entre 1 e ${numeroMaximo[fase-1]} `);
-        if (chute == numeroSecreto) {
-        break;
+        if (chute == null){
+            break;
         } else {
-            if (chute > numeroSecreto){
-                alert(`O número secreto é menor que ${chute}`);
-            } else {
-                alert(`O número secreto é maior que ${chute}`);
+            if (Math.sign(chute) != 1 || Math.sign(chute) == 0){
+                alert('Digite um número válido');
+            } else { 
+                if (chute == numeroSecreto) {
+                    break;
+                } else {
+                    if (chute > numeroSecreto){
+                        alert(`O número secreto é menor que ${chute}`);
+                    } else {
+                        alert(`O número secreto é maior que ${chute}`);
+                    }
+                }
             }
+            tentativas++;
         }
-        tentativas++;
     }
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-    novaFase = fase + 1;
-    continuar = prompt(`Deseja passar para a fase ${novaFase}? Digite Sim ou Não.`)
-    totalTentativas = totalTentativas + tentativas;
-    if(continuar == 'Sim'){
-        fase++;
-        tentativas = 1;
-    }else {break;}
+    if(chute != null){
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+        novaFase = fase + 1;
+        continuar = prompt(`Deseja passar para a fase ${novaFase}? Digite Sim ou Não.`)
+        totalTentativas = totalTentativas + tentativas;
+        if(continuar == 'Sim'){
+            fase++;
+            tentativas = 1;
+        }else {break;}
+    }
 }
-let palavraFases = fase > 1? 'fases' :'fase'
-let palavraTentativa = totalTentativas > 1? 'tentativas' : 'tentativa'
-alert(`No total você usou ${totalTentativas} ${palavraTentativa} e passou por ${fase} ${palavraFases}.`)
-
-document.getElementById("titulo").innerHTML = 'Resultado do jogo!'
-document.getElementById("mensagem").innerHTML = `No total você usou ${totalTentativas} ${palavraTentativa} e passou por ${fase} ${palavraFases}.`
+if (chute == null) {
+    document.getElementById("titulo").innerHTML = 'Você não quis jogar :/ Recarre a página se mudar de ideia! (;'
+}else {
+    let palavraFases = fase > 1? 'fases' :'fase'
+    let palavraTentativa = totalTentativas > 1? 'tentativas' : 'tentativa'
+    document.getElementById("titulo").innerHTML = 'Resultado do jogo:'
+    document.getElementById("mensagem").innerHTML = `No total você usou ${totalTentativas} ${palavraTentativa} e passou por ${fase} ${palavraFases}. Atualize a página para jogar mais!`
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 O curso não propôs nenhum desafio específico. Então, eu resolvi me desafiar. 
 
-A proposta é criar uma página html para o jogo de adivinhar o número que tenha fases, ou seja, após adivinhar o número o jogador pode escolher por ir para uma fase mais dificil. 
+## A proposta é criar uma página html para o jogo de adivinhar o número que tenha fases, ou seja, após adivinhar o número o jogador pode escolher por ir para uma fase mais dificil. 
+
+## Além disso existe tratamento caso o usuário cancele algum dos alerts que deveria inserir um número ou insira zero, um número negativo ou letra. 
